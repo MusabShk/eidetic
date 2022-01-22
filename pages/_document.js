07b3164b621f4cedc427211af4ea481c42053abf
Musab Shaikh <musabshaikh172@gmail.com>
2022-01-22 22:49:19 +0530

    final update

--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;600&family=Satisfy&display=swap"
             rel="stylesheet"
